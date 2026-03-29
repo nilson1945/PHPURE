183e1d952bb63f1b17bd8257f39c4070b4a0fca7
nilson1945 <pupet1943@gmail.com>
2026-03-29 09:31:41 -0300

    phpuro

--- configuracao/public/clients/v1/add_clients/index1.php
+++ configuracao/public/clients/v1/add_clients/index1.php
@@ -2,7 +2,5 @@
 $httpVerb = $_SERVER ["REQUEST_METHOD"];
 echo 'adicionar novo clientes' . PHP_EOL;
 echo "METHODO : $httpVerb";
-echo 'Vai buscar dados de um clientes';
-
 
 ?>
--- configuracao/public/clients/v1/all_clients/index1.php
+++ configuracao/public/clients/v1/all_clients/index1.php
@@ -1,4 +1,7 @@
 <?php
+$httpVerb = $_SERVER ["REQUEST_METHOD"];
+echo 'adicionar novo clientes' . PHP_EOL;
+echo "METHODO : $httpVerb";
 echo 'Vai listar todos os clientes';
 
 
