4a79f708ff9b09bb87e06bc3e891bc016bc8a2cf
nilson1945 <pupet1943@gmail.com>
2026-04-08 21:35:39 -0300

    phpuro

---
+++ configuracao/config_clients_v1.php
@@ -0,0 +1,17 @@
+<?php
+define('MYSQL', [
+    'type' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'clients',
+    'username' => 'meu_client',
+    'password' => '0*Njksm_61c[qKy4' // a senha que você criou
+
+]);
+
+
+
+
+
+
+
+?>
--- configuracao/public/clients/v1/add_clients/index1.php
+++ configuracao/public/clients/v1/add_clients/index1.php
@@ -1,25 +1,24 @@
 <?php
+require_once __DIR__ . "/../../../../../configuracao/config_clients_v1.php";
 require_once __DIR__ . "/../../../../../inc/init.php";
 Api::checkHTTPMethod('POST');
-/*
-$httpVerb = $_SERVER["REQUEST_METHOD"];
 
-if ($httpVerb !== 'POST') {
-    http_response_code(405);
-
-    echo json_encode([
-        'status' => 'error',
-        'code' => 405,
-        'message' => 'Method Not Allowed',
-    ], JSON_PRETTY_PRINT);
-
-    exit;
+// Evita erro quando não existe REQUEST_METHOD
+if (!isset($_SERVER["REQUEST_METHOD"])){
+    $_SERVER["REQUEST_METHOD"] = 'POST';
 }
 
-echo 'Adicionar novo cliente' . PHP_EOL;
-echo "MÉTODO: $httpVerb";*/
-echo 'ok';
+use Medoo\Medoo;
 
+// Conexão com o banco
+$database = new Medoo(MYSQL);
+
+// Seleciona todos os registros da tabela correta
+$results = $database->select('meu_client', '*');
+var_dump($results);
+
+header('Content-Type: application/json');
+echo json_encode($results);
 
 
 ?>
